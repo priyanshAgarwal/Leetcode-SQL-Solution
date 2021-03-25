e2567198be642c5c2d9787b4e1c9a6d0404791f9
Priyansh Agarwal <priyansh1990@gmail.com>
2021-03-24 23:05:04 -0400

    1270. All People Report to the Given Manager

--- 1270. All People Report to the Given Manager.sql
+++ 1270. All People Report to the Given Manager.sql
@@ -11,11 +11,13 @@ Table: Employees
 | manager_id    | int     |
 +---------------+---------+
 employee_id is the primary key for this table.
-Each row of this table indicates that the employee with ID employee_id and name employee_name reports his work to his/her direct manager with manager_id
+Each row of this table indicates that the employee with ID employee_id and name employee_name reports
+ his work to his/her direct manager with manager_id
 The head of the company is the employee with employee_id = 1.
  
 
-Write an SQL query to find employee_id of all employees that directly or indirectly report their work to the head of the company.
+Write an SQL query to find employee_id of all employees that directly or indirectly report their 
+work to the head of the company.
 
 The indirect relation between managers will not exceed 3 managers as the company is small.
 
@@ -53,3 +55,15 @@ The employee with employee_id 4 report his work indirectly to the head of the co
 The employee with employee_id 7 report his work indirectly to the head of the company 7 --> 4 --> 2 --> 1.
 The employees with employee_id 3, 8 and 9 don't report their work to head of company directly or indirectly. 
 */
+
+WITH CTE AS (
+    SELECT employee_id from 
+    Employees where manager_id=1 and employee_id!=1
+    UNION ALL
+    select E.employee_id FROM
+    CTE C INNER JOIN Employees E ON E.manager_id=C.employee_id
+)
+
+SELECT employee_id 
+FROM CTE 
+OPTION (MAXRECURSION 3);
---
+++ 1303. Find the Team Size.sql
@@ -0,0 +1,54 @@
+/*
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| employee_id   | int     |
+| team_id       | int     |
++---------------+---------+
+employee_id is the primary key for this table.
+Each row of this table contains the ID of each employee and their respective team.
+Write an SQL query to find the team size of each of the employees.
+
+Return result table in any order.
+
+The query result format is in the following example:
+
+Employee Table:
++-------------+------------+
+| employee_id | team_id    |
++-------------+------------+
+|     1       |     8      |
+|     2       |     8      |
+|     3       |     8      |
+|     4       |     7      |
+|     5       |     9      |
+|     6       |     9      |
++-------------+------------+
+Result table:
++-------------+------------+
+| employee_id | team_size  |
++-------------+------------+
+|     1       |     3      |
+|     2       |     3      |
+|     3       |     3      |
+|     4       |     1      |
+|     5       |     2      |
+|     6       |     2      |
++-------------+------------+
+Employees with Id 1,2,3 are part of a team with team_id = 8.
+Employees with Id 4 is part of a team with team_id = 7.
+Employees with Id 5,6 are part of a team with team_id = 9.
+
+*/
+
+-- Method 1
+SELECT employee_id, B.team_size FROM Employee A
+LEFT JOIN
+(SELECT team_id, COUNT(team_id) AS team_size   FROM Employee GROUP BY 1) AS B
+ON A.team_id=B.team_id;
+
+-- Method 2
+/*
+Remember there is no group by in OVER clause its always Partition by or Order By when using DENSE_RANK()
+*/
+SELECT employee_id, COUNT(team_id) OVER(PARTITION BY team_id) AS TEAM_SIZE FROM Employee;
