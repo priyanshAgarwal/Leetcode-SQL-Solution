8cebde6c38aae261598495279c1f799462d486c7
Priyansh Agarwal <priyansh1990@gmail.com>
2021-03-27 01:34:08 -0400

    1699. Number of Calls Between Two Persons

---
+++ 1355. Activity Participants.sql
@@ -0,0 +1,75 @@
+/*
+Table: Friends
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
+| activity      | varchar |
++---------------+---------+
+id is the id of the friend and primary key for this table.
+name is the name of the friend.
+activity is the name of the activity which the friend takes part in.
+Table: Activities
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key for this table.
+name is the name of the activity.
+ 
+
+Write an SQL query to find the names of all the activities with neither maximum, nor minimum number of participants.
+
+Return the result table in any order. Each activity in table Activities is performed by any person in the table Friends.
+
+The query result format is in the following example:
+
+Friends table:
++------+--------------+---------------+
+| id   | name         | activity      |
++------+--------------+---------------+
+| 1    | Jonathan D.  | Eating        |
+| 2    | Jade W.      | Singing       |
+| 3    | Victor J.    | Singing       |
+| 4    | Elvis Q.     | Eating        |
+| 5    | Daniel A.    | Eating        |
+| 6    | Bob B.       | Horse Riding  |
++------+--------------+---------------+
+
+Activities table:
++------+--------------+
+| id   | name         |
++------+--------------+
+| 1    | Eating       |
+| 2    | Singing      |
+| 3    | Horse Riding |
++------+--------------+
+
+Result table:
++--------------+
+| activity     |
++--------------+
+| Singing      |
++--------------+
+
+Eating activity is performed by 3 friends, maximum number of participants, (Jonathan D. , Elvis Q. and Daniel A.)
+Horse Riding activity is performed by 1 friend, minimum number of participants, (Bob B.)
+Singing is performed by 2 friends (Victor J. and Jade W.)
+*/
+
+SELECT L1.NUM AS ConsecutiveNums  
+FROM
+Logs L1,
+Logs L2,
+Logs L3
+WHERE L1.ID=L2.ID-1 -- First two lines are to get consecutive we have to do                    
+AND L2.ID=L3.ID-1   -- ID and next two lines to get the numbers
+AND L1.NUM=L2.NUM
+AND L2.NUM=L3.NUM;
+
+
