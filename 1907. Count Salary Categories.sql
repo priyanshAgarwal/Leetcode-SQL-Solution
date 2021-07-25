29a365868cd33fc9149db1124578e0cb059d612f
Priyansh Agarwal <priyansh1990@gmail.com>
2021-07-25 17:21:41 -0400

    1364. Number of Trusted Contacts of a Customer

---
+++ 1907. Count Salary Categories.sql
@@ -0,0 +1,60 @@
+/*
+
+1907. Count Salary Categories
+
+Table: Accounts
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| account_id  | int  |
+| income      | int  |
++-------------+------+
+account_id is the primary key for this table.
+Each row contains information about the monthly income for one bank account.
+ 
+
+Write an SQL query to report the number of bank accounts of each salary category. The salary categories are:
+
+"Low Salary": All the salaries strictly less than $20000.
+"Average Salary": All the salaries in the inclusive range [$20000, $50000].
+"High Salary": All the salaries strictly greater than $50000.
+The result table must contain all three categories. If there are no accounts in a category, then report 0. Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Accounts table:
++------------+--------+
+| account_id | income |
++------------+--------+
+| 3          | 108939 |
+| 2          | 12747  |
+| 8          | 87709  |
+| 6          | 91796  |
++------------+--------+
+
+Result table:
++----------------+----------------+
+| category       | accounts_count |
++----------------+----------------+
+| Low Salary     | 1              |
+| Average Salary | 0              |
+| High Salary    | 3              |
++----------------+----------------+
+
+Low Salary: Account 2.
+Average Salary: No accounts.
+High Salary: Accounts 3, 6, and 8.
+
+
+*/
+
+-- Very Important but wierd concept
+
+SELECT 'Low Salary' AS Category, SUM(income <20000) AS accounts_count FROM Accounts
+UNION ALL 
+SELECT 'Average Salary' AS Category, SUM(income BETWEEN 20000 AND 50000) AS accounts_count FROM Accounts
+UNION ALL
+SELECT 'High Salary' AS Category, SUM(income>50000) AS accounts_count FROM Accounts;
