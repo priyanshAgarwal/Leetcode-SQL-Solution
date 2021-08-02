85ba00bafe53957415b4fd5bbfd4c4c2043fc1e5
Priyansh Agarwal <priyansh1990@gmail.com>
2021-08-02 04:49:02 -0400

    626. Exchange Seats

--- New File - Copy copy 2.sql
+++ New File - Copy copy 2.sql
@@ -1,5 +1,13 @@
 /*
 
 
-
+["TEAM_NAME", "GOAL_FOR", "GOAL_AGAINST", "POINTS"],
+["Ajax", 0, 1, 0], 
+["Ajax", 3, 3, 1], 
+["Dortmund", 5, 2, 3], 
+["Arsenal", 0, 0, 1],
+["Dortmund", 1, 0, 3], 
+["Arsenal", 3, 3, 1], 
+["Ajax", 2, 5, 0], 
+["Ajax", 0, 0, 1]
 */
