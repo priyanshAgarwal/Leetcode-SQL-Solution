e2392037e685280115a1020d3431cc61b1442668
Priyansh Agarwal <priyansh1990@gmail.com>
2021-08-16 19:58:53 -0400

    602. Friend Requests II: Who Has the Most Friends

--- 602. Friend Requests II. Who Has the Most Friends.sql
+++ 602. Friend Requests II. Who Has the Most Friends.sql
@@ -2,7 +2,8 @@
 
 602. Friend Requests II: Who Has the Most Friends
 
-In social network like Facebook or Twitter, people send friend requests and accept others' requests as well.
+In social network like Facebook or Twitter, people send friend requests and accept others' 
+requests as well.
 
  
 
@@ -16,13 +17,15 @@ Table request_accepted
 | 2            | 3           | 2016-06-08 |
 | 3            | 4           | 2016-06-09 |
 +--------------+-------------+------------+
-This table holds the data of friend acceptance, while requester_id and accepter_id both are the id of a person.
+This table holds the data of friend acceptance, while requester_id and accepter_id both 
+are the id of a person.
  
 
 Write a query to find the the people who has most friends and the most friends number under the following rules:
 
 It is guaranteed there is only 1 people having the most friends.
-The friend request could only been accepted once, which mean there is no multiple records with the same requester_id and accepter_id value.
+The friend request could only been accepted once, which mean there is no multiple records 
+with the same requester_id and accepter_id value.
 For the sample data above, the result is:
 
 Result table:
@@ -31,9 +34,13 @@ Result table:
 |------|------|
 | 3    | 3    |
 +------+------+
-The person with id '3' is a friend of people '1', '2' and '4', so he has 3 friends in total, which is the most number than any others.
+
+The person with id '3' is a friend of people '1', '2' and '4', so he has 3 friends in total, 
+which is the most number than any others.
+
 Follow-up:
-In the real world, multiple people could have the same most number of friends, can you find all these people in this case?
+In the real world, multiple people could have the same most number of friends, 
+can you find all these people in this case?
 
  WITH REQUEST AS (SELECT requester_id, COUNT(requester_id) AS C_R FROM request_accepted
  GROUP BY requester_id),
@@ -48,7 +55,6 @@ In the real world, multiple people could have the same most number of friends, c
  LIMIT 1;
 
 Completely wrong logic and missed test case.
-
 
 */
 
