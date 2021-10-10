daa67ac88818c7bf847ebe04ba521130599936bd
Priyansh Agarwal <priyansh1990@gmail.com>
2021-10-10 15:27:35 -0400

    Counting Instances in Text

---
+++ Pivot the Occupation column.sql
@@ -0,0 +1,44 @@
+/*
+
+Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
+
+Note: Print NULL when there are no more names corresponding to an occupation.
+
+Input Format
+
+The OCCUPATIONS table is described as follows:
+
+
+
+Occupation will only contain one of the following values: Doctor, Professor, Singer or Actor.
+
+Sample Input
+
+
+
+Sample Output
+
+Jenny    Ashley     Meera  Jane
+Samantha Christeen  Priya  Julia
+NULL     Ketty      NULL   Maria
+Explanation
+
+The first column is an alphabetically ordered list of Doctor names.
+The second column is an alphabetically ordered list of Professor names.
+The third column is an alphabetically ordered list of Singer names.
+The fourth column is an alphabetically ordered list of Actor names.
+The empty cell data for columns with less than the maximum number of names per occupation (in this case, the Professor and Actor columns) are filled with NULL values.
+
+*/
+
+-- Method 1 Using Pivot Table
+SELECT
+    Doctor,Professor,Singer,Actor
+FROM
+(
+    SELECT ROW_NUMBER() OVER (PARTITION BY OCCUPATION ORDER BY NAME) RowNumber,* FROM OCCUPATIONS
+) AS tempTable
+PIVOT
+(
+    MIN(NAME) FOR OCCUPATION IN (Doctor,Professor,Singer,Actor)
+) AS pivotTable
