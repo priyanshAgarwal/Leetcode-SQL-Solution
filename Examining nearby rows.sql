8434cbe8ae32e8068d1a3616b93ab6533c07b17b
Priyansh Agarwal <priyansh1990@gmail.com>
2022-01-10 02:19:18 -0800

    Examining nearby rows

---
+++ Examining nearby rows.sql
@@ -0,0 +1,22 @@
+/*
+
+https://www.windowfunctions.com/questions/over/2
+
+Question:
+The cats would like to see the average of the weight of them, the cat just after them and the cat just before them.
+
+The first and last cats are content to have an average weight of consisting of 2 cats not 3.
+
+Return: name, weight, average_weight
+Order by: weight
+
+
+
+*/
+
+select 
+    name,
+    weight,
+     avg(weight) over (order by weight ROWS between 1 preceding and 1 following) as average_weight
+from cats order by weight
+
