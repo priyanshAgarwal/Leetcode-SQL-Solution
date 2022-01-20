a36be989bed8fc38f5b041db0576dfc63b536d0e
Priyansh Agarwal <priyansh1990@gmail.com>
2022-01-19 22:16:46 -0800

    The Most Popular Client_Id Among Users Using Video and Voice Calls

---
+++ The Most Popular Client_Id Among Users Using Video and Voice Calls.sql
@@ -0,0 +1,7 @@
+/*
+
+The Most Popular Client_Id Among Users Using Video and Voice Calls
+Select the most popular client_id based on a count of the number of users who have at least 50% of their events from the following list: 'video call received', 'video call sent', 'voice call received', 'voice call sent'.
+
+
+*/
