751140fa8f866309dce092c47db00a4d6913bfa8
Priyansh Agarwal <priyansh1990@gmail.com>
2022-01-23 07:33:36 -0800

    Submission Types

---
+++ Submission Types.sql
@@ -0,0 +1,18 @@
+/*
+
+Submission Types
+Write a query that returns the user ID of all users that have created at least one ‘Refinance’ submission and at least one ‘InSchool’ submission.
+
+
+*/
+
+with cte as (
+select user_id, 
+    sum(case when type='Refinance' then 1 else 0 end) as Refinance,
+    sum(case when type='InSchool' then 1 else 0 end) as InSchool
+from loans
+where type in ('Refinance','InSchool')
+group by user_id
+)
+
+select user_id from cte where Refinance>=1 and InSchool>=1
