41ac0dae8823fc2c1d19aa2d9d7c9ec083a38143
Priyansh Agarwal <priyansh1990@gmail.com>
2022-02-02 01:45:06 -0800

    Olympic Medals By Chinese Athletes

--- Olympic Medals By Chinese Athletes.sql
+++ Olympic Medals By Chinese Athletes.sql
@@ -4,6 +4,7 @@ Find the number of medals earned in each category by Chinese athletes from the 2
 
 Look Again
 Made Mistake
+Very Important Concept
 */
 
 WITH CTE AS (SELECT medal, 
@@ -17,4 +18,22 @@ FROM olympics_athletes_events
 WHERE team = 'China'
 GROUP BY medal)
 
-SELECT * FROM CTE
+SELECT * FROM CTE;
+
+-- Method 2
+-- Very Important Concept
+WITH MEDAL_LIST AS (
+SELECT DISTINCT medal,
+COUNT(*) FILTER(WHERE YEAR = 2000) over (partition by medal) as medals_2000,
+COUNT(*) FILTER(WHERE YEAR = 2004) over (partition by medal) as medals_2004,
+COUNT(*) FILTER(WHERE YEAR = 2008) over (partition by medal) as medals_2008,
+COUNT(*) FILTER(WHERE YEAR = 2012) over (partition by medal) as medals_2012,
+COUNT(*) FILTER(WHERE YEAR = 2016) over (partition by medal) as medals_2016,
+COUNT(*) OVER(partition by medal) AS total_medals
+FROM olympics_athletes_events
+WHERE medal IS NOT NULL
+AND team = 'China'
+
+)
+
+SELECT * FROM MEDAL_LIST
