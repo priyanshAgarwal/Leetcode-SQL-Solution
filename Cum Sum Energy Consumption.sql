7b6f4ecc9939313ec14cc16971b3afab16eb2799
Priyansh Agarwal <priyansh1990@gmail.com>
2022-04-17 23:31:23 -0700

    178. Rank Scores

--- Cum Sum Energy Consumption.sql
+++ Cum Sum Energy Consumption.sql
@@ -1,9 +1,6 @@
 /*
 Cum Sum Energy Consumption
 Calculate the running total (i.e., cumulative sum) energy consumption of the Meta/Facebook data centers in all 3 continents by the date. Output the date, running total energy consumption, and running total percentage rounded to the nearest whole number.
-
-
-
 */
 
 with fb_energy as (
--- New File copy 20.sql
+++ New File copy 20.sql
@@ -4,25 +4,3 @@
 
 */
 
-
-with cte as (
-select 
-    pe_description, count(*), dense_rank() over(order by count(*) desc) rnk
-from 
-    los_angeles_restaurant_health_inspections
-where
-    facility_name ilike '%cafe%' or facility_name ilike '%tea%' or facility_name ilike '%juice%'
-group by
-    1)
-
-
-select
-    distinct facility_name
-from 
-    los_angeles_restaurant_health_inspections
-where
-    (facility_name ilike '%cafe%' or facility_name ilike '%tea%' or facility_name ilike '%juice%')
-    and
-    pe_description in (select distinct pe_description from cte where rnk = 3)
-    
-
