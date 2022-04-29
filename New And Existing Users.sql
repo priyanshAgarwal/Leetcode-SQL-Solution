1bda512457a24c316e10f2858e325cc4c2febd90
Priyansh Agarwal <priyansh1990@gmail.com>
2022-04-28 19:25:16 -0700

    New And Existing Users

--- New And Existing Users.sql
+++ New And Existing Users.sql
@@ -1,25 +1,48 @@
 /*
 
 New And Existing Users
-Calculate the share of new and existing users. Output the month, share of new users, and share of existing users as a ratio.
-New users are defined as users who started using services in the current month. Existing users are users who started using services in current month and used services in any previous month. 
+
+
+Interview Question Date: February 2021
+
+Microsoft
+Hard
+Active Interview
+ID 2028
+1
+0
+Calculate the share of new and existing users for each month in the table. Output the month, share of new users, and share of existing users as a ratio.
+New users are defined as users who started using services in the current month (there is no usage history in previous months). Existing users are users who used services in current month, but they also used services in any previous month. 
 Assume that the dates are all from the year 2020.
+
 
 To Do Again (Difficult Question) Got Confused
 */
 
 
-WITH CTE_A AS (
-    SELECT DISTINCT USER_ID, EXTRACT(MONTH FROM TIME_ID) AS MONTH FROM FACT_EVENTS 
-),
-
-CTE_B AS (
-    SELECT USER_ID, MONTH ,DENSE_RANK() OVER(PARTITION BY USER_ID ORDER BY MONTH) AS USE_RANK FROM CTE_A
+-- Method 1
+WITH CTE AS (
+    SELECT USER_ID, MONTH(TIME_ID) AS TIME_MONTH ,DENSE_RANK() OVER(PARTITION BY USER_ID ORDER BY MONTH(TIME_ID)) AS LOGIN_RANK FROM fact_events
 )
 
-SELECT MONTH, 
-    SUM(CASE WHEN USE_RANK=1 THEN 1 ELSE 0 END)::DECIMAL/COUNT(*) AS share_new_users,
-    SUM(CASE WHEN USE_RANK<>1 THEN 1 ELSE 0 END)::DECIMAL/COUNT(*) AS share_existing_users FROM CTE_B
-    GROUP BY 1
-    ORDER BY 1;
+SELECT TIME_MONTH, 
+    COUNT(DISTINCT CASE WHEN LOGIN_RANK=1 THEN USER_ID ELSE NULL END)*1.0/COUNT(DISTINCT USER_ID) AS NEW_USER_COUNT,
+    COUNT(DISTINCT CASE WHEN LOGIN_RANK<>1 THEN USER_ID ELSE NULL END)*1.0/COUNT(DISTINCT USER_ID) AS EXISTING_USER_COUNT
+FROM CTE
+GROUP BY 1;
     
+
+-- Method 2
+WITH CTE AS (
+    SELECT 
+        USER_ID,
+        MONTH(TIME_ID) AS CURRENT_MONTH,
+        MIN(MONTH(TIME_ID)) OVER(PARTITION BY USER_ID ORDER BY MONTH(TIME_ID)) AS FIRST_MONTH FROM fact_events
+)
+
+SELECT 
+    CURRENT_MONTH,
+    COUNT(DISTINCT CASE WHEN CURRENT_MONTH=FIRST_MONTH THEN USER_ID ELSE NULL END)*1.0/COUNT(DISTINCT USER_ID) AS NEW_USER,
+    COUNT(DISTINCT CASE WHEN CURRENT_MONTH!=FIRST_MONTH THEN USER_ID ELSE NULL END)*1.0/COUNT(DISTINCT USER_ID) AS OLD_USER
+FROM CTE
+GROUP BY 1
