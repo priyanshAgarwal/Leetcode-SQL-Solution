764c8ed96a0fda0509ffcaab0eafa62a6496fac0
Priyansh Agarwal <priyansh1990@gmail.com>
2022-04-28 23:11:58 -0700

    Calculate a rolling average of total revenue

--- Calculate a rolling average of total revenue.sql
+++ Calculate a rolling average of total revenue.sql
@@ -1,4 +1,5 @@
 /*
+Calculate a rolling average of total revenue
 
 For each day in August 2012, calculate a rolling average of total revenue over the previous 15 days. Output should contain date and revenue columns, sorted by the date. Remember to account for the possibility of a day having zero revenue. This one's a bit tough, so don't be afraid to check out the hint!
 
@@ -7,25 +8,6 @@ https://pgexercises.com/questions/aggregates/rollingavg.html
 
 
 */
-WITH CTE AS (SELECT * FROM (select
-			cast(generate_series(timestamp '2012-07-10', '2012-08-31','1 day') as date) as date
-		)  as dategen
-		left outer join
-			-- left join to a table of per-day revenue
-			(select cast(bks.starttime as date) as date,
-				sum(case
-					when memid = 0 then slots * facs.guestcost
-					else slots * membercost
-				end) as rev
-
-				from cd.bookings bks
-				inner join cd.facilities facs
-					on bks.facid = facs.facid
-				group by cast(bks.starttime as date)
-			) as revdata
-			on dategen.date = revdata.date)
-			
-			
 WITH CTE AS (
 SELECT A.DATE, COALESCE(REVENUE_DAILY,0) AS REVENUE_DAILY FROM (
   SELECT cast(generate_series(timestamp '2012-07-10', '2012-08-31','1 day') as date) AS DATE)
@@ -37,8 +19,14 @@ SELECT A.DATE, COALESCE(REVENUE_DAILY,0) AS REVENUE_DAILY FROM (
 FROM CD.BOOKINGS A
 INNER JOIN CD.FACILITIES B
 ON A.FACID=B.FACID
-GROUP BY 1) B
+GROUP BY 1
+ORDER BY 1) B
 ON A.DATE=B.DATE)
+
+SELECT * FROM(SELECT DATE,
+		AVG(REVENUE_DAILY) over(order by date rows 14 preceding) as avgrev
+FROM CTE) AS A
+WHERE A.DATE>='2012-08-01'
 
 
 
