c91db7094455fc585d7ec19ce041b4fd7ff09596
Priyansh Agarwal <priyansh1990@gmail.com>
2022-05-27 20:24:29 -0700

    1225. Report Contiguous Dates

--- 1225. Report Contiguous Dates.sql
+++ 1225. Report Contiguous Dates.sql
@@ -134,26 +134,3 @@ FROM CTE
 GROUP BY DATE_SUB(DATE,INTERVAL SEQ DAY),STATE
 ORDER BY 2
 
-
-with a as (
-(select fail_date as date,
-'failed' as period_state
-from failed
-)
-union all
-(select success_date as date,
-'succeeded' as period_state
-from succeeded)
-),
-
-b as (
-select date,
-period_state,
-dense_rank() over (partition by period_state order by date asc) as seq
-from a where date between '2019-01-01' and '2019-12-31'
-)
-
-select period_state, min(date) as start_date, max(date) as end_date from b
-group by DATE_SUB(date,INTERVAL seq DAY),period_state
-order by start_date asc
-
