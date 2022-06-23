d0e67ecfe8239c142865bae715014bde38a640f7
Priyansh Agarwal <priyansh1990@gmail.com>
2022-06-22 23:20:57 -0700

    183. Customers Who Never Order

---
+++ 183. Customers Who Never Order.sql
@@ -0,0 +1,80 @@
+/*
+
+183. Customers Who Never Order
+Easy
+
+1126
+
+82
+
+Add to List
+
+Share
+SQL Schema
+Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID and name of a customer.
+ 
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key column for this table.
+customerId is a foreign key of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+ 
+
+Write an SQL query to report all customers who never order anything.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+*/
+
+SELECT NAME AS Customers 
+FROM CUSTOMERS A
+LEFT JOIN ORDERS B
+ON A.ID=B.CUSTOMERID
+WHERE CUSTOMERID IS NULL
