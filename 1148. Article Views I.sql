30b2e911a92e6d197820e95b806bb09bda375e47
Priyansh Agarwal <priyansh1990@gmail.com>
2022-06-24 00:31:41 -0700

    1068. Product Sales Analysis I

---
+++ 1148. Article Views I.sql
@@ -0,0 +1,70 @@
+/*
+
+1148. Article Views I
+Easy
+
+159
+
+19
+
+Add to List
+
+Share
+SQL Schema
+Table: Views
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key for this table, it may have duplicate rows.
+Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+Note that equal author_id and viewer_id indicate the same person.
+ 
+
+Write an SQL query to find all the authors that viewed at least one of their own articles.
+
+Return the result table sorted by id in ascending order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Views table:
++------------+-----------+-----------+------------+
+| article_id | author_id | viewer_id | view_date  |
++------------+-----------+-----------+------------+
+| 1          | 3         | 5         | 2019-08-01 |
+| 1          | 3         | 6         | 2019-08-02 |
+| 2          | 7         | 7         | 2019-08-01 |
+| 2          | 7         | 6         | 2019-08-02 |
+| 4          | 7         | 1         | 2019-07-22 |
+| 3          | 4         | 4         | 2019-07-21 |
+| 3          | 4         | 4         | 2019-07-21 |
++------------+-----------+-----------+------------+
+Output: 
++------+
+| id   |
++------+
+| 4    |
+| 7    |
++------+
+
+*/
+
+SELECT DISTINCT A.AUTHOR_ID AS ID FROM VIEWS A
+INNER JOIN VIEWS B
+ON A.VIEWER_ID=B.AUTHOR_ID AND A.ARTICLE_ID=B.ARTICLE_ID
+ORDER BY 1
+
+SELECT DISTINCT author_id as id
+from views
+WHERE author_id = viewer_id
+ORDER BY id
