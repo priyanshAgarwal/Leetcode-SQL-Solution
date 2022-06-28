b9708b2827b6c17b68da82d613c6bdd71db3a78c
Priyansh Agarwal <priyansh1990@gmail.com>
2022-06-27 19:09:12 -0700

    1965. Employees With Missing Information

---
+++ 1965. Employees With Missing Information.sql
@@ -0,0 +1,94 @@
+/*
+1965. Employees With Missing Information
+Easy
+
+171
+
+12
+
+Add to List
+
+Share
+SQL Schema
+Table: Employees
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the name of the employee whose ID is employee_id.
+ 
+
+Table: Salaries
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the salary of the employee whose ID is employee_id.
+ 
+
+Write an SQL query to report the IDs of all the employees with missing information. The information of an employee is missing if:
+
+The employee's name is missing, or
+The employee's salary is missing.
+Return the result table ordered by employee_id in ascending order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employees table:
++-------------+----------+
+| employee_id | name     |
++-------------+----------+
+| 2           | Crew     |
+| 4           | Haven    |
+| 5           | Kristian |
++-------------+----------+
+Salaries table:
++-------------+--------+
+| employee_id | salary |
++-------------+--------+
+| 5           | 76071  |
+| 1           | 22517  |
+| 4           | 63539  |
++-------------+--------+
+Output: 
++-------------+
+| employee_id |
++-------------+
+| 1           |
+| 2           |
++-------------+
+Explanation: 
+Employees 1, 2, 4, and 5 are working at this company.
+The name of employee 1 is missing.
+The salary of employee 2 is missing.
+
+
+*/
+
+WITH CTE AS (
+SELECT A.EMPLOYEE_ID FROM EMPLOYEES A
+INNER JOIN SALARIES B
+ON A.EMPLOYEE_ID=B.EMPLOYEE_ID),
+
+
+EMP_UNION AS (
+    SELECT EMPLOYEE_ID FROM EMPLOYEES
+    UNION ALL
+    SELECT EMPLOYEE_ID FROM SALARIES
+)
+
+SELECT EMPLOYEE_ID FROM EMP_UNION WHERE EMPLOYEE_ID NOT IN (SELECT EMPLOYEE_ID FROM CTE)
+ORDER BY 1
