6f80a9b230593b56e59487baa31cbec8fbf8df4c
Priyansh Agarwal <priyansh1990@gmail.com>
2022-06-29 01:07:41 -0700

    1407. Top Travellers

--- 1407. Top Travellers.sql
+++ 1407. Top Travellers.sql
@@ -92,3 +92,11 @@ Donald did not have any rides, the distance traveled by him is 0.
 
 
 */
+
+select u.name, ifnull(sum(r.distance),0) as travelled_distance 
+from users u
+left join
+rides r
+on u.id = r.user_id
+group by u.id
+order by travelled_distance desc, u.name
