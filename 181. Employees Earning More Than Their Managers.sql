fabc9a9d97973b10a305b3c39c25ffe372c8c886
Priyansh Agarwal <priyanshAgarwal@users.noreply.github.com>
2024-02-28 23:47:22 -0800

    181. Employees Earning More Than Their Managers

--- 181. Employees Earning More Than Their Managers.sql
+++ 181. Employees Earning More Than Their Managers.sql
@@ -47,3 +47,30 @@ INNER JOIN EMPLOYEE B
 ON A.ID=B.MANAGERID
 WHERE B.SALARY>A.SALARY
 
+-- Pandas
+
+import pandas as pd
+
+def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
+
+    result_df = pd.merge(employee, employee, left_on='managerId', right_on='id', how='inner').rename(   
+        columns = 
+            {'name_x':'employee_name',
+            'salary_x':'employee_salary',
+            'name_y':'manager_name',
+            'salary_y':'manager_salary'})[['employee_name','employee_salary','manager_name','manager_salary']]
+    
+    emp=result_df[result_df['employee_salary']>result_df['manager_salary']][['employee_name']].rename(columns={'employee_name':'Employee'})
+    return emp
+
+
+import pandas as pd
+
+def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
+    emp = pd.merge(employee, employee, 
+                left_on='managerId', 
+                right_on='id', 
+                how='inner')\
+            .query("salary_x>salary_y")[['name_x']]\
+            .rename(columns={'name_x':'Employee'})
+    return emp
